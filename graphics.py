17f8fea2d9029fa60263c44986045feb9ee6c310
fernando8franco <francoduranluisfernando@gmail.com>
2024-12-26 21:09:31 -0600

    Cell class added & Window, Point and Line classes group in graphics.py

--- graphics.py
+++ graphics.py
@@ -1,5 +1,21 @@
 from tkinter import Tk, BOTH, Canvas
-from line import Line
+
+class Point:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+
+    def __init__(self, p1: Point, p2: Point):
+        self.__p1 = p1
+        self.__p2 = p2
+
+    def draw(self, canvas: Canvas, fill_color: str):
+        canvas.create_line(
+            self.__p1.x, self.__p1.y, self.__p2.x, self.__p2.y, fill=fill_color, width=2
+        )
 
 class Window:
     def __init__(self, width, height):
@@ -12,8 +28,8 @@ class Window:
         
 
     def redraw(self):
-        self.__root.update()
         self.__root.update_idletasks()
+        self.__root.update()
 
     def draw_line(self, line: Line, fill_color="black"):
         line.draw(self.__canvas, fill_color)
--- main.py
+++ main.py
@@ -1,11 +1,13 @@
-from window import Window
-from point import Point
-from line import Line
+from graphics import Window
+from cell import Cell
 
 def main():
     win = Window(800, 600)
-    line = Line(Point(0, 1), Point(200, 200))
-    win.draw_line(line)
+
+    cell = Cell(win)
+    cell.has_left_wall = False
+    cell.draw(50, 50, 100, 100)
+
     win.wait_for_close()
 
 if __name__=="__main__":
