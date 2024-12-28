2218d2809fc48dba1c043966abaca808becb5423
fernando8franco <francoduranluisfernando@gmail.com>
2024-12-27 22:20:16 -0600

    draw move method added

--- cell.py
+++ cell.py
@@ -33,3 +33,20 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        line = Line(
+                Point(
+                    (self._x2 + self._x1) / 2, 
+                    (self._y2 + self._y1) / 2
+                ), 
+                Point(
+                    (to_cell._x2 + to_cell._x1) / 2, 
+                    (to_cell._y2 + to_cell._y1) / 2
+                    )
+                )
+
+        if undo:
+            self._win.draw_line(line, "gray")
+        else:
+            self._win.draw_line(line, "red")
--- main.py
+++ main.py
@@ -5,8 +5,11 @@ def main():
     win = Window(800, 600)
 
     cell = Cell(win)
-    cell.has_left_wall = False
+    cell2 = Cell(win)
     cell.draw(50, 50, 100, 100)
+    cell2.draw(100, 50, 150, 100)
+
+    cell.draw_move(cell2)
 
     win.wait_for_close()
 
